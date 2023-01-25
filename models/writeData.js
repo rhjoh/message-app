2f7e08b085623184cfd79b1ea523ab466cc1c87a
Rhys <rhys.johnston00@gmail.com>
2023-01-25 21:40:04 +1100

    Cleanup

--- models/writeData.js
+++ models/writeData.js
@@ -10,11 +10,7 @@ function writeData(data){
     const jsonFile = fs.readFileSync('./messages.json', function(error){
         console.log(error)
     })
-    /* console.log("writeData() model happening: ") */
     let newArray = JSON.parse(jsonFile)
-    /* console.log(typeof(newArray))
-    console.log(newArray)
-    console.log("writeData() end, new messages:") */
     newArray.push(data)
 
     fs.writeFileSync('./messages.json', JSON.stringify(newArray))
